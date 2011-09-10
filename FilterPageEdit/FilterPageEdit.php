cfb6d3a3a80e0638a15502cf209f3fcc9f03be98
Robert Munteanu <robert@lmn.ro>
2011-09-10 22:43:10 +0300

    Initial commit
    Right now this project handles displaying input fields for the custom
    fields specified, without being able to submit them.

---
+++ FilterPageEdit/FilterPageEdit.php
@@ -0,0 +1,72 @@
+<?php
+# Copyright (c) 2011 Robert Munteanu (robert@lmn.ro)
+
+# Filter page edit for MantisBT is free software: 
+# you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, 
+# either version 2 of the License, or (at your option) any later version.
+#
+# Filter page edit plugin for MantisBT is distributed in the hope 
+# that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Filter page edit plugin for MantisBT.  
+# If not, see <http://www.gnu.org/licenses/>.
+
+class FilterPageEditPlugin extends MantisPlugin {
+    
+    static $SUPPORTED_FIELD_TYPES = array ( CUSTOM_FIELD_TYPE_NUMERIC , CUSTOM_FIELD_TYPE_STRING, CUSTOM_FIELD_TYPE_FLOAT );
+    
+    public function register() {
+        $this->name = plugin_lang_get("title");
+        $this->description = plugin_lang_get("description");
+
+        $this->version = "1.0";
+        $this->requires = array(
+			"MantisCore" => "1.2.6",
+			"jQueryUI" => "1.8"
+        );
+
+        $this->author = "Robert Munteanu";
+        $this->contact = "robert@lmn.ro";
+    }
+    
+    public function hooks() {
+        
+        return array(
+            'EVENT_LAYOUT_RESOURCES' => 'resources'
+        );
+    }
+    
+    public function resources( $p_event ) {
+        
+        
+        if ( basename( $_SERVER['SCRIPT_NAME'] ) != 'view_all_bug_page.php')
+            return;
+        
+        $t_custom_field_ids = custom_field_get_linked_ids( helper_get_current_project() );
+        $t_editable_custom_field_ids = array();
+        
+        foreach ( $t_custom_field_ids as $t_custom_field_id ) {
+            
+            $t_custom_field_defintion = custom_field_get_definition( $t_custom_field_id );
+            
+            if ( !in_array( $t_custom_field_defintion['type'], self::$SUPPORTED_FIELD_TYPES ) ) 
+                continue;
+            
+            $t_editable_custom_field_ids[] = $t_custom_field_id;
+        }
+        
+        if ( count ( $t_editable_custom_field_ids ) == 0 )
+            return;
+        
+        $t_field_ids_string = implode(',', $t_editable_custom_field_ids );
+        
+        return '<script type="text/javascript" src="' . plugin_page( 'filter-page-edit.php&amp;fields=' . $t_field_ids_string ) . '"></script>'.
+            '<link rel="stylesheet" type="text/css" href="'. plugin_file('filter-page-edit.css') .'"></link>';
+    }    
+}
+
+?>
---
+++ FilterPageEdit/pages/filter-page-edit.php
@@ -0,0 +1,54 @@
+<?php 
+header ("Content-Type: text/javascript");
+
+require_once 'core.php';
+
+$t_icon_path = config_get( 'icon_path' );
+$t_edit_icon_string = '<img class="start-inline-edit" border="0" width="16" height="16" src="' . $t_icon_path . 'update.png"/>';
+?>
+var FilterPageEdit = {
+    installCustomFieldEdit : function(fieldName) {
+        var bugTable = jQuery("#buglist");
+        var headerRow = bugTable.find("tr.row-category");
+        var customFieldColumn = headerRow.find("td:contains(" + fieldName + ")");
+        var customFieldColumnIndex = headerRow.children().index(customFieldColumn);
+        customFieldColumn.append('<?php echo $t_edit_icon_string ?>');
+        
+        var editableColumns = [];
+        
+        bugTable.find('input[type=checkbox]').each(function() {
+            var bugRow = jQuery(this).parent().parent();
+            var editableColumn = bugRow.find('td:eq('+customFieldColumnIndex+')')
+            editableColumn.addClass('inline-editable').click(function() {
+                FilterPageEdit._makeEditable(jQuery(this));
+            });
+            
+            editableColumns[editableColumns.length] = editableColumn;
+        });
+        
+        customFieldColumn.find('.start-inline-edit').click(function() {
+            for ( var i = 0 ; i < editableColumns.length; i++ ) {
+                FilterPageEdit._makeEditable(editableColumns[i]);
+            }
+            jQuery(this).unbind('click');
+        });
+    },
+    
+    _makeEditable: function(jQueryCell) {
+        var oldText = jQueryCell.text();
+        jQueryCell.removeClass('inline-editable');
+        jQueryCell.text('').append('<input type="text" value="' + oldText + '">');
+        jQueryCell.unbind('click');
+    }
+};
+jQuery(document).ready(function() {
+<?php
+
+$f_custom_fields = explode( ',' , gpc_get_string( 'fields' ) );
+foreach ( $f_custom_fields as $t_custom_field_id ) {
+    $t_custom_field = custom_field_get_definition( $t_custom_field_id );
+    echo "\tFilterPageEdit.installCustomFieldEdit('" . $t_custom_field['name'] ."');\n";
+}
+?>
+});
+    
