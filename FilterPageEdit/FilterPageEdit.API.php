0c732255671bf17a38d18c113f71bb7f11408bb9
Robert Munteanu <robert@lmn.ro>
2011-09-11 11:41:14 +0300

    Add missing copyright headers

--- FilterPageEdit/FilterPageEdit.API.php
+++ FilterPageEdit/FilterPageEdit.API.php
@@ -1,4 +1,19 @@
 <?php 
+# Copyright (c) 2011 Robert Munteanu (robert@lmn.ro)
+
+# Filter page edit for MantisBT is free software:
+# you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation,
+# either version 2 of the License, or (at your option) any later version.
+#
+# Filter page edit plugin for MantisBT is distributed in the hope
+# that it will be useful, but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Filter page edit plugin for MantisBT.
+# If not, see <http://www.gnu.org/licenses/>.
 
 class FilterPageEditDao {
 
